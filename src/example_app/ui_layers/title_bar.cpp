e2de3f80c12784d15f9373ba7f834d28fb89d17d
fabianperdomolaguna <faperdomola@gmail.com>
2025-09-12 09:53:08 -0500

    Top right corner round for close button - Change icon maximize and restore

--- src/example_app/ui_layers/title_bar.cpp
+++ src/example_app/ui_layers/title_bar.cpp
@@ -24,27 +24,38 @@ void DrawWindowButtons(
 	float button_size,
 	float icon_size,
     ImU32 button_color,
-	float horizontal_offset,
-    const std::function<void()>& on_click = nullptr)
+    const std::function<void()>& on_click = nullptr,
+    bool round_top_right = false)
 {
     ImRect button_rect(button_start, button_end);
     bool button_hovered = button_rect.Contains(ImGui::GetMousePos());
     if (button_hovered)
-        ImGui::GetBackgroundDrawList()->AddRectFilled(button_start, button_end, button_color);
+    {
+        if (round_top_right)
+        {
+            ImGui::GetBackgroundDrawList()->AddRectFilled(
+                button_start, button_end, button_color,
+                12.0f, ImDrawFlags_RoundCornersTopRight);
+        }
+        else 
+        {
+            ImGui::GetBackgroundDrawList()->AddRectFilled(button_start, button_end, button_color);
+        }
+    }
     if (button_hovered && ImGui::IsMouseClicked(0))
 		on_click();
 
     ImVec2 center = button_rect.GetCenter();
-    ImVec2 icon_start = ImVec2(center.x - icon_size * 0.5f, center.y - icon_size * 0.5f);
-    ImVec2 icon_end = ImVec2(icon_start.x + icon_size, icon_start.y + icon_size);
+    ImVec2 icon_start = ImVec2(floor(center.x - icon_size * 0.5f), floor(center.y - icon_size * 0.5f));
+    ImVec2 icon_end = ImVec2(floor(icon_start.x + icon_size), floor(icon_start.y + icon_size));
     ImGui::GetBackgroundDrawList()->AddImage(
         icon_texture,
         icon_start,
         icon_end
     );
 
-	button_start = ImVec2(button_start.x + button_size + horizontal_offset, button_start.y);
-    button_end = ImVec2(button_end.x + button_size + horizontal_offset, button_end.y);
+	button_start = ImVec2(button_start.x + button_size, button_start.y);
+    button_end = ImVec2(button_end.x + button_size, button_end.y);
 }
 
 bool IsMaximized(GLFWwindow* window)
@@ -61,6 +72,7 @@ export class TitleBar : public Layer
     std::unique_ptr<ImageTexture> minimize_button;
     std::unique_ptr<ImageTexture> maximize_button;
     std::unique_ptr<ImageTexture> close_button;
+	std::unique_ptr<ImageTexture> restore_button;
 
 public:
     TitleBar(Application* app) : m_app(app) 
@@ -71,6 +83,7 @@ public:
         minimize_button = std::make_unique<ImageTexture>(g_minimize_button, g_minimize_button_len, GL_RGBA, true);
         maximize_button = std::make_unique<ImageTexture>(g_maximize_button, g_maximize_button_len, GL_RGBA, true);
         close_button = std::make_unique<ImageTexture>(g_close_button, g_close_button_len, GL_RGBA, true);
+		restore_button = std::make_unique<ImageTexture>(g_restore_button, g_restore_button_len, GL_RGBA, true);
     }
 
     void OnRender() override
@@ -119,12 +132,11 @@ public:
             ImVec2(top_left.x + logo_offset.x + logo_size, top_left.y + logo_offset.y + logo_size)
 		);
 
-        float horizontal_offset = 0.5f;
         float button_size = 35.0f;
         float icon_size = 14.0f;
 
         ImGuiViewport* viewport = ImGui::GetMainViewport();
-        ImVec2 button_start = ImVec2(viewport->Pos.x + viewport->Size.x - button_size * 3 - horizontal_offset * 2, viewport->Pos.y);
+        ImVec2 button_start = ImVec2(viewport->Pos.x + viewport->Size.x - button_size * 3, viewport->Pos.y);
 		ImVec2 button_end = ImVec2(button_start.x + button_size, button_start.y + title_bar_height);
 
         DrawWindowButtons(
@@ -134,18 +146,18 @@ public:
             button_size,
             icon_size,
             IM_COL32(154, 140, 152, 128),
-            horizontal_offset,
             [&]() { glfwIconifyWindow(m_app->m_window->m_window); }
         );
 
         DrawWindowButtons(
             button_start,
             button_end,
-            (ImTextureID)(intptr_t)maximize_button->get_texture(),
+            (ImTextureID)(intptr_t)(
+                IsMaximized(m_app->m_window->m_window) ? restore_button->get_texture()
+                : maximize_button->get_texture()),
             button_size,
             icon_size,
             IM_COL32(154, 140, 152, 128),
-            horizontal_offset,
             [&]() {
                 if (IsMaximized(m_app->m_window->m_window))
                     glfwRestoreWindow(m_app->m_window->m_window);
@@ -161,8 +173,8 @@ public:
             button_size,
             icon_size,
             IM_COL32(255, 0, 0, 128),
-            horizontal_offset,
-            [&]() { m_app->m_window->m_close_popup = true; }
+            [&]() { m_app->m_window->m_close_popup = true; },
+            !IsMaximized(m_app->m_window->m_window)
         );
     }
 
